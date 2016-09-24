da2cbc0022261ef5639d1e181ebeee9cc089bb30
Johann Montfort <jmonfu@gmail.com>
2016-09-24 08:25:31 +0100

    simple pokemon display

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,10 +1,47 @@
 import { Component } from '@angular/core';
 
+import { PokedexService } from './pokedex.service'
+import { Pokemon } from './pokemon'
+
 @Component({
   selector: 'app-root',
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css']
 })
 export class AppComponent {
-  title = 'app works!';
+
+  //initialize list
+  pokemon: Pokemon[] = [];
+
+  isLoading: boolean = false;
+  error: boolean = true;
+
+  //Inject Pokemon service
+  constructor(private pokedexService:PokedexService) {}
+  
+  ngOnInit(){
+    //Load initial data
+    this.loadMore();
+  }
+
+  loadMore(){
+    this.isLoading = true;
+
+    //use the pokemon service to load the next 9 pokemons
+    this.pokedexService.getPokemon(this.pokemon.length, 9)
+      .then(pokemon => {
+        pokemon = pokemon.map(p => {
+          p.imageLoaded = false;
+          return p;
+        });
+        //if loading was successful, we append the result to the list
+        this.pokemon = this.pokemon.concat(pokemon);
+        this.isLoading = false;
+        this.error = false;
+      })
+      .catch(() => {
+        this.error = true;
+        this.isLoading = false;
+      })
+  }
 }
